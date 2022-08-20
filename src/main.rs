29d896a54da607f3b434842ebacd8f9d76fbdad1
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2022-08-20 18:26:23 +0500

    books api

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,39 @@
 #[macro_use]
 extern crate diesel;
-#[macro_use]
-extern crate diesel_codegen;
+
 extern crate dotenv;
 
 use dotenv::dotenv;
 use std::env;
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
+
+use crate::models::Book;
 
 mod schema;
 mod models;
 
 fn main() {
-    println!("Hello API");
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("set DATABASE_URL");
+    let conn = PgConnection::establish(&database_url).unwrap();
+
+    let book = models::NewBook {
+        title: String::from("Shades of flowers"),
+        author: String::from("Zaryab"),
+        published: true,
+    };
+
+    if models::Book::insert(book, &conn) {
+        println!("success");
+        
+    } else {
+        println!("failed");
+    }
+
+    let booki = models::Book::all(&conn);
+    for i in booki.iter() {
+        println!("ID: {} , Title: {}, Author: {}", i.id, i.title, i.author);
+    }
 }
