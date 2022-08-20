d65f9af89dad59827dd40bfa8149078c9a83a4f2
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2022-08-20 11:38:45 +0500

    diesel setup

---
+++ src/models.rs
@@ -0,0 +1,13 @@
+use diesel;
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
+
+
+
+#[derive(Queryable)]
+pub struct Book {
+    pub id: i32,
+    pub title: String,
+    pub author: String,
+    pub published: bool,
+}
